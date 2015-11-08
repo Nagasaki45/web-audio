b518020451c4b87d28bedebc8bc0ef7f530929ef
Tom Gurion <nagasaki45@gmail.com>
2015-11-08 18:54:16 +0200

    Support unix-socket configuration using command line argument

--- tornado_main.py
+++ tornado_main.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
-import tornado.options
+from tornado.options import define, parse_command_line, options
 import tornado.httpserver
 import tornado.websocket
 import tornado.ioloop
 import tornado.web
+import tornado.netutil
 
-tornado.options.define('port', type=int, default=8000)
+define('port', type=int, default=8000)
+define('unix_socket', default=False)
 
 
 class MainHandler(tornado.web.RequestHandler):
@@ -33,7 +35,7 @@ class WsHandler(tornado.websocket.WebSocketHandler):
 
 
 def main():
-    tornado.options.parse_command_line()
+    parse_command_line()
 
     handlers = (
         (r'/', MainHandler),
@@ -43,7 +45,11 @@ def main():
     settings = {'static_path': 'static'}
     tornado_app = tornado.web.Application(handlers, **settings)
     server = tornado.httpserver.HTTPServer(tornado_app)
-    server.listen(tornado.options.options.port)
+    if options.unix_socket:
+        socket = tornado.netutil.bind_unix_socket('/tmp/web-audio.sock')
+        server.add_socket(socket)
+    else:
+        server.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
 
 
