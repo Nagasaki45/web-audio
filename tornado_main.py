767251480fbae6c6bb59254acf842444a4087210
Tom Gurion <nagasaki45@gmail.com>
2015-11-08 15:05:32 +0200

    Drop django dependencies. Still broken

--- tornado_main.py
+++ tornado_main.py
@@ -1,70 +1,49 @@
 #!/usr/bin/env python
 
-import os
-
-from tornado.options import options, define, parse_command_line
+import tornado.options
 import tornado.httpserver
 import tornado.websocket
 import tornado.ioloop
 import tornado.web
-import tornado.wsgi
 
-import django.core.handlers.wsgi
-from django.conf import settings as djsettings
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                      'web_audio.settings.base')
-
-define('port', type=int, default=8000)
-
-clients = []
+tornado.options.define('port', type=int, default=8000)
 
 
-class HelloHandler(tornado.web.RequestHandler):
-
+class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write('Hello from tornado')
+        self.render('index.html')
 
 
 class WsHandler(tornado.websocket.WebSocketHandler):
+    clients = []
 
     def open(self):
-        clients.append(self)
+        self.clients.append(self)
 
     def on_close(self):
-        clients.remove(self)
+        self.clients.remove(self)
 
     def on_message(self, message):
-        '''
+        """
         Sends the message to all of the clients except the sender.
-        '''
-
-        for c in clients:
+        """
+        for c in self.clients:
             if c != self:
                 c.write_message(message)
 
 
 def main():
     tornado.options.parse_command_line()
-    wsgi_app = tornado.wsgi.WSGIContainer(
-        django.core.handlers.wsgi.WSGIHandler()
-    )
 
     handlers = (
-        (r'/hello-tornado', HelloHandler),
-        (djsettings.WEBSOCKET_URL, WsHandler),
-        (r'.*', tornado.web.FallbackHandler, dict(fallback=wsgi_app)),
+        (r'/', MainHandler),
+        (r'/ws', WsHandler),
     )
 
-    settings = dict(
-        debug=djsettings.DEBUG,
-        # be sure to run "python manage.py collecstatic"!
-        static_path=djsettings.STATIC_ROOT
-    )
-
+    settings = {'static_path': 'static'}
     tornado_app = tornado.web.Application(handlers, **settings)
     server = tornado.httpserver.HTTPServer(tornado_app)
-    server.listen(options.port)
+    server.listen(tornado.options.options.port)
     tornado.ioloop.IOLoop.instance().start()
 
 
